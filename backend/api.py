6322bb6f2ff8efccc37c3b7a7e8ca0a97f9bf711
Patrick <patrykjaroszewski@proton.me>
2023-09-27 22:47:58 +0000

    Added more stub routes

--- backend/api.py
+++ backend/api.py
@@ -9,6 +9,7 @@ from models.ingredient import Ingredient
 from models.recipe import Recipe
 
 from mongo_db import RecipeDatabase
+from datetime import date
 
 app = FastAPI()
 
@@ -51,7 +52,10 @@ def get_recipe(id: int) -> Recipe:
     recipe = Recipe.from_external_data(result)
 
     # Save recipe to database?
-    print(app.db.create(recipe))
+    insert = app.db.create(recipe)
+    print(insert)
+
+    return recipe
 
 
 # Functions related to packaged grocery products
@@ -104,10 +108,98 @@ def get_ingredient(id: int) -> Ingredient:
     result = spoonacular.get_ingredient(id)
     return Ingredient.from_external_data(result)
 
-# Functions related to displaying ingredients
+
+# Functions related to retrieving user's data
 # -----------------------------------------------------
 
+@app.get("/user/{user}/recipes")
+def retrieve_users_recipes(user: str):
+    """Retrieves a user's stored recipes. 
+    
+    Arguments:
+    user -- username
+
+    ! Should add a way of authentication !
+    
+    TO BE IMPLEMENTED
+    """
+    ...
+
+@app.get("/user/{user}/ingredients")
+def retrieve_users_ingredients(user: str):
+    """Retrieves a user's stored ingredients .
+    
+    Arguments:
+    user -- username
+
+    TO BE IMPLEMENTED
+    """
+    ...
 
 
-# Functions related to saving ingredients to database
+
+# Functions related to saving to database
 # -----------------------------------------------------
+
+@app.post("/user/{user}/ingredient")
+def add_ingredient(user: str, ingredient: str, expiry_date: date=None):
+    """ Adds an ingredient to the user's list of ingredients. 
+    
+    Arguments:
+    user -- username
+    ingredient -- query parameter, ingredient name
+    expiry_date -- the date of expiry (can be none)
+
+    Returns:
+    ?
+
+    TO BE IMPLEMENTED
+    """
+    ...
+
+@app.delete("/user/{user}/ingredient")
+def delete_ingredient(user: str, ingredient: str):
+    """Deletes an ingredient from the user's list of ingredients
+    
+    Arguments:
+    user -- username
+    name -- the name of the ingredient to be deleted
+
+    TO BE IMPLEMENTED
+    """
+    ...
+
+
+@app.get("user/{user}/recipe")
+def add_recipe(user: str, spn_id: int):
+    """ Adds a recipe to user's recipe list given the Spoonacular id.
+    
+    user -- username 
+    spn_id -- the spoonacular id of the recipe
+
+    TO BE IMPLEMENTED
+    """
+    ...
+
+@app.post("/user/{user}/recipe")
+def create_recipe(user: str, ingredient: str):
+    """Creates a custom recipe and adds it to the user's recipe list.
+    
+    Arguments:
+    user -- username
+    name -- 
+
+    TO BE IMPLEMENTED
+    """
+    ...
+
+@app.delete("/user/{user}/recipe")
+def delete_recipe(user: str, id: int):
+    """Deletes a recipe from the user's saved recipes. 
+    
+    Arguments:
+    id -- the id of the recipe to be deleted
+
+    TO BE IMPLEMENTED
+    """
+
